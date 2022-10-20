d948cbb8f680d9ccc5ec28d7bcbe81274d0937a3
custersnele <custersnele@gmail.com>
2022-10-20 11:43:45 +0200

    Update Robbery.java

--- src/main/java/be/pxl/ja/robbery/Robbery.java
+++ src/main/java/be/pxl/ja/robbery/Robbery.java
@@ -2,9 +2,9 @@ package be.pxl.ja.robbery;
 
 public class Robbery {
     public static void main(String[] args) {
-        Knapsack knapsack = new Knapsack(35);
+        /*Knapsack knapsack = new Knapsack(35);
 
-        Shop shop = new Shop();
+        List<Product> shop = new ArrayList<>();
         shop.add(new Product("stereo", 30, 3000));
         shop.add(new Product("laptop", 20, 2000));
         shop.add(new Product("guitar", 15, 1500));
@@ -14,6 +14,6 @@ public class Robbery {
         System.out.println("\nList of products:");
         for (Product product : knapsack.getItems()) {
             System.out.println(product);
-        }
+        }*/
     }
 }
